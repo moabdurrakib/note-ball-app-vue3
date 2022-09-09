bab06a6f05f6f218e782812d562b986407bdcc38
mabdurrakib <mabdurrakib.dev@gmail.com>
2022-09-09 12:03:39 +0600

    router install and setup

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -19,7 +25,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <style scoped>
 header {
-  line-height: 1.5;
+  line-height: 2;
 }
 
 .logo {
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHistory,
+    routes: []
+})
+
+createApp(App)
+    .use(router)
+    .mount('#app')
