85cf742d95af341a99c80b89fb87c25d9138e34a
mabdurrakib <mabdurrakib.dev@gmail.com>
2022-09-11 21:20:44 +0600

    adding a routes

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
+import ViewNotes from '@/views/ViewNotes.vue'
 
 import './assets/main.css'
 
 const router = createRouter({
     history: createWebHistory(),
-    routes: []
+    routes: [
+        {
+            path: '/',
+            name: 'notes',
+            component: ViewNotes
+        }
+    ]
 })
 
 createApp(App)
