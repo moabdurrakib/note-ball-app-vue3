06e20922c568192d3472f147d8fc69d18f2cc50c
mabdurrakib <mabdurrakib.dev@gmail.com>
2022-09-21 19:40:49 +0600

    Add Note form (desgin)

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="notes ms-2">
-    <div class="form-floating m-2">
+    <div class="form-floating bg-success p-4 mb-5">
       <textarea
         class="form-control"
         placeholder="Add a new note"
         id="floatingTextarea"
       ></textarea>
       <label for="floatingTextarea"></label>
-      <button type="button" class="btn btn-primary m-2">Primary</button>
-      <button type="button" class="btn btn-primary m-2">Primary</button>
+      <div class="d-flex jus flex-row-reverse">
+        <button
+          type="button"
+          class="btn btn-primary text-white bg-success p-2 text-dark bg-opacity-25"
+        >
+          Add new notes
+        </button>
+      </div>
     </div>
     <div class="card" v-for="i in 3" :key="i">
       <div class="card-body">
@@ -16,8 +22,10 @@
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </p>
-        <a href="#" class="card-link">Card link</a>
-        <a href="#" class="card-link">Another link</a>
+        <footer class="d-flex justify-content-evenly">
+          <a href="#" class="card-link">Edit</a>
+          <a href="#" class="card-link">Delete</a>
+        </footer>
       </div>
     </div>
   </div>
